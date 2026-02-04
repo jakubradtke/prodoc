bc32ea920b16fbd0cdc222aa8d5d6d013a1dc388
Jakub Radtke <jakub.radtke@graidtech.com>
2026-02-04 17:31:28 +0100

    Update build scripts
    Signed-off-by: Jakub Radtke <jakub.radtke@graidtech.com>

--- build.bat
+++ build.bat
@@ -1,26 +1,4 @@
-@echo off
+rem @echo off
 
-set "cmd_dir=%~dp0"
-if "%cmd_dir:~-1%"=="\" set "cmd_dir=%cmd_dir:~0,-1%"
-
-set "python_cmd=%PRODOC_PYTHON%"
-
-echo %PATH% | "C:\Windows\System32\find.exe" /i "%cmd_dir%" > nul
-if %errorlevel% neq 0 (
-    echo ERROR: The directory %cmd_dir% is not in the PATH.
-    exit /b 1
-)
-
-if not defined python_cmd (
-    echo ERROR: PRODOC_PYTHON is not set.
-    exit /b 1
-)
-if not exist "%python_cmd%" (
-    echo ERROR: PRODOC_PYTHON="%python_cmd%" does not point to an existing file.
-    exit /b 1
-)
-
-:call_build
-call %PRODOC_PYTHON% %PRODOC_DIR%\pm_tools\scripts\mmd2doc.py %*
+call %PRODOC_PYTHON%\python.exe %PRODOC_HOME%\pm_tools\scripts\mmd2doc.py %*
 exit /b
-
---
+++ build_all.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "ROOT=%CD%"
+
+for /r "%ROOT%" %%F in (*.mmd) do (
+    set "name=%%~nF"
+    rem skip if first char is "_"
+    if /i not "!name:~0,1!"=="_" (
+        echo Processing %%F
+        call build.bat "%%F"
+    )
+)
+
+endlocal
